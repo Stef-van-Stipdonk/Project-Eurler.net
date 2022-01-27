1e9ce8a7e39f2a6b04e8f18281370a778385f11a
Stef <stefvanstipdonk@gmail.com>
2022-01-27 18:04:30 +0100

    Problem_7

---
+++ Project-Euler/Project_7/Program.cs
@@ -0,0 +1,30 @@
+﻿
+List<int> primes = new List<int>();
+
+int number = 2;
+while (primes.Count < 10_001)
+{
+    CheckPrime();
+    number++;
+}
+
+void CheckPrime()
+{
+    if(number == 2)
+        primes.Add(number);
+
+    if(number % 2 == 0)
+        return;
+
+    var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+    for (int i = 3; i <= boundary; i += 2)
+    {
+        if(number % i == 0)
+            return;
+    }
+    
+    primes.Add(number);
+}
+
+primes.ForEach(c => Console.WriteLine(c));
