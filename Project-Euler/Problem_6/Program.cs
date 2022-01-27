11c45d7a0f44ec071220b0bc39a4e18984093e85
Stef <stefvanstipdonk@gmail.com>
2022-01-27 13:29:55 +0100

    Problem 6 solved

---
+++ Project-Euler/Problem_6/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int sumOfSquares = 0;
+int squareOfSum = 0;
+
+for (var i = 0; i < 101; i++)
+{
+       sumOfSquares += i * i;
+       squareOfSum += i;
+}
+
+squareOfSum = squareOfSum * squareOfSum;
+
+Console.WriteLine($"{squareOfSum} + {sumOfSquares} = {squareOfSum - sumOfSquares}");
