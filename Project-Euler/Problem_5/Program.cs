5a98b9f5f7de61899ab2a1f505a438986145d886
Stef <stefvanstipdonk@gmail.com>
2022-01-27 13:21:37 +0100

    Problem 5 solved

---
+++ Project-Euler/Problem_5/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int number = 1;
+var numberFound = false;
+while (!numberFound)
+{
+    for (var i = 1; i < 21; i++)
+    {
+        if (number % i != 0)
+        {
+            number++;
+            break;
+        }
+
+        if (i == 20)
+        {
+            Console.WriteLine(number);
+            numberFound = true;
+        }
+    }
+}
