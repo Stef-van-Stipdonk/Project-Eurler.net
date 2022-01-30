95280fe3586535e94f154e06ad985a048b89f4df
Stef <stefvanstipdonk@gmail.com>
2022-01-30 21:49:11 +0100

    Problem_9

---
+++ Project-Euler/Problem_9/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// Solution writeup: 
+// Pick two numbers m and n, the larger number should be m and the smaller n
+// Calculate a, b and c as follows
+// a = m to the power of 2 - n to the power of 2
+// b = (m * n)2
+// c = m to the power of 2 + n to the power of 2
+// a2 + b2 = c2
+
+var productOfTripletToBeFound = 1000;
+
+int a;
+
+for (a = 1; a <= productOfTripletToBeFound / 3; a++)
+{
+    int b;
+    for (b = a + 1; b <= productOfTripletToBeFound / 2; b++)
+    {
+        int c = productOfTripletToBeFound - a - b;
+        if ((a * a) + (b * b) == (c * c))
+        {
+            Console.WriteLine($"{a} + {b} = {c}");
+            Console.WriteLine($"Product: {a * b * c}");
+        }
+    }
+}
