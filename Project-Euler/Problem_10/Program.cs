1ac46defdce4f54de24abcc4e2c63c50199f9869
Stef <stefvanstipdonk@gmail.com>
2022-01-30 22:05:29 +0100

    Problem_10

---
+++ Project-Euler/Problem_10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Numerics;
+
+List<Int64> primes = new List<Int64>();
+
+int number = 2;
+
+while (true)
+{
+    CheckPrime();
+    number++;
+
+    if (primes.Last() > 2_000_000)
+    {
+        primes.Remove(primes.Last());
+        break;
+    }
+}
+
+Console.WriteLine(primes.Sum());
+
+void CheckPrime()
+{
+    if(number == 2)
+        primes.Add(number);
+
+    if (number % 2 == 0)
+        return;
+
+    var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+    for (int i = 3; i <= boundary; i += 2)
+    {
+        if(number % i == 0)
+            return;
+    }
+
+    primes.Add(number);
+}
