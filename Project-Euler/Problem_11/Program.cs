c0c4cf96228062f9d949416e28fe6d801b9494fb
Stef <stefvanstipdonk@gmail.com>
2022-02-04 15:56:17 +0100

    Problem_11

---
+++ Project-Euler/Problem_11/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using Problem_8;
+
+var numbers = new GenerateNumbers().GetNumberArray();
+
+ulong greatestProduct = 0;
+
+for (var i = 0; i < numbers.Count; i++)
+{
+    for (int j = 0; j < numbers[i].Count; j++)
+    {
+        ulong product = 0;
+        
+        if (numbers.Count - 1 > i + 3 && numbers[i].Count - 1 > j + 3)
+        {
+            var p1 = (ulong)numbers[i][j];
+            var p2 = (ulong)numbers[i + 1][j + 1];
+            var p3 = (ulong)numbers[i + 2][j + 2];
+            var p4 = (ulong)numbers[i + 3][j + 3];
+            
+            product = p1 * p2 * p3 * p4;
+        }
+
+        if (j - 2 > 0 && numbers.Count - 1 > i + 3)
+        {
+            var p1 = (ulong)numbers[i][j];
+            var p2 = (ulong)numbers[i + 1][j - 1];
+            var p3 = (ulong)numbers[i + 2][j - 2];
+            var p4 = (ulong)numbers[i + 3][j - 3];
+
+            if (product < p1 * p2 * p3 * p4)
+                product = p1 * p2 * p3 * p4;
+        }
+
+        if (greatestProduct < product)
+            greatestProduct = product;
+    }
+}
+
+Console.WriteLine(greatestProduct);
